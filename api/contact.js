05e0ef286da94fd228795712624fa447fa749d45
MalonsoFrey <miss.dunaewa201141@gmail.com>
2025-05-18 19:33:17 +0400

    server testing

--- api/contact.js
+++ api/contact.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    const { name } = req.body;
-    console.log(`Thank you for your interest, ${name}`);
+    const { name, email, message } = req.body;
+    console.log("Received data:", { name, email, message });
     res.status(200).json({ message: `Thank you for your interest, ${name}` });
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
